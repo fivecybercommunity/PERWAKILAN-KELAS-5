f89be82e1f0e2a42d59edc23ed6499882fe8ef8b
nardaiv <92304461+nardaiv@users.noreply.github.com>
2022-07-30 21:39:21 +0700

    menambahkan selection di bagian proker

--- scripts/proker.js
+++ scripts/proker.js
@@ -1,19 +1,52 @@
-let i = 1;
+// let i = 1;
 
-function action(panah) {
-    document.getElementById("hal-" + i).style.display = "none";
+// function action(panah) {
+//     document.getElementById("hal-" + i).style.display = "none";
 
-    if (panah === "kiri") {
-        i--;
-        if (i <=0) {
-            i = 3;
-        }
+//     if (panah === "kiri") {
+//         i--;
+//         if (i <=0) {
+//             i = 3;
+//         }
+//     }
+//     else if (panah === "kanan") {
+//         i++;
+//         if (i > 3) {
+//             i = 1;
+//         }
+//     }
+//     document.getElementById("hal-" + i).style.display = "flex";
+// }
+const selector = document.querySelector('#content-selector');
+let slides = document.getElementsByClassName('slide');
+
+hide(slides[selector.value]);
+hideAll();
+
+
+
+function hideAll(){
+    for(let index of slides){
+        hide(index);
     }
-    else if (panah === "kanan") {
-        i++;
-        if (i > 3) {
-            i = 1;
-        }
+}
+
+function hide(target){
+    if(target!=undefined){
+        target.classList.toggle("d-flex");
+        target.classList.toggle("d-none");
     }
-    document.getElementById("hal-" + i).style.display = "flex";
+}
+
+function contentSwitch(){
+    let keyValue = selector.value;
+    
+    if(keyValue != undefined){
+        for(let slide of slides){
+            if (slide.classList.contains('d-flex')){
+                hide(slide);
+            }
+        }
+        hide(slides[keyValue]);
+    }
 }
